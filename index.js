c40a120429c2ab32fe7f89f5d0eed4fc59707427
UnShame <mrunshame@gmail.com>
2019-02-03 22:11:44 +0300

    Write scripts, add configs, add graphics etc. etc.

---
+++ index.js
@@ -0,0 +1,49 @@
+const notifier = require('node-notifier');
+
+module.exports = async function pomodoro(
+    totalSteps = 25,
+    stepDuration = 1000 * 60,
+    stepName = 'minute'
+) {
+    notifier.notify({
+        title: `Do thing for ${pluralize(stepName, totalSteps)}.`,
+        message: 'Or else!',
+        icon: './icons/icon_work.png'
+    });
+
+    await runInterval(totalSteps, stepDuration, (stepCount) => {
+        console.log(`Still ${pluralize(stepName, totalSteps - stepCount)} to go.`);
+    });
+
+    notifier.notify({
+        title: `Okay, you can rest now.`,
+        message: `But I'm still watching you.`,
+        icon: './icons/icon_rest.png'
+    });
+};
+
+function runInterval(totalSteps, stepDuration, stepCallback) {
+
+    let stepCount = 0;
+    stepCallback(stepCount);
+
+    return new Promise((resolve) => {
+        let interval = setInterval(() => {
+
+            stepCount++;
+
+            if (stepCount == totalSteps) {
+                clearInterval(interval);
+                return void resolve();
+            }
+            else {
+                stepCallback(stepCount);
+            }
+
+        }, stepDuration);
+    });
+}
+
+function pluralize(word, number) {
+    return `${number} ${word}${number === 1 ? '' : 's'}`;
+}
